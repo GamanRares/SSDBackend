218c92f034e2cfd939b09d96a8cf948860fb037c
rares <rares_gamanrares@yahoo.com>
2018-11-28 19:00:33 +0200

    Project initialization

---
+++ src/main/java/SSDBackend/BackingBeans/BusinessLogic/BusinessLogic.java
@@ -0,0 +1,17 @@
+package SSDBackend.BackingBeans.BusinessLogic;
+
+import lombok.Data;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+
+@Data
+@Stateless
+public class BusinessLogic implements Serializable {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+}
